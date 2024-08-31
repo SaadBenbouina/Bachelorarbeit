3410415a35ae5fa621644fbba462f7e2e4142395
saadbenboujina <saadbenboujina@gmail.com>
2024-08-31 14:24:46 +0200

    train model

--- Model.py
+++ Model.py
@@ -4,4 +4,11 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=70)  # train the model
+results =model.train(
+data="config.yaml",epochs=10,
+imgsz=640,
+name="boat_detection_model",
+project="/var/folders/3m/k2m2bg694w15lfb_1kz6blvh0000gn/T/wzQL.Cf1otW/Bachelorarbeit",
+cache=True,
+save_period=1  # Saves the model after each epoch
+)
