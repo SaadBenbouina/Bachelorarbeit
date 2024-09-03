def736ea1b8b76504cd6663b88d49b8d79f05c8d
saadbenboujina <saadbenboujina@gmail.com>
2024-09-04 00:58:00 +0200

    ..

--- Model.py
+++ Model.py
@@ -3,10 +3,11 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("/var/folders/3m/k2m2bg694w15lfb_1kz6blvh0000gn/T/wzQL.Cf1otW/Bachelorarbeit/boat_detection_yolo_model/weights/best.pt")  # build a new model from scratch
 # Use the model
+
 results =model.train(
-data="config.yaml",epochs=5,
+data="config.yaml",epochs=15,
 imgsz=640,
-name="boat_detection_yolo_model",
+name="boat_detection_yolo_model_new",
 project="/var/folders/3m/k2m2bg694w15lfb_1kz6blvh0000gn/T/wzQL.Cf1otW/Bachelorarbeit",
 cache=True,
 save_period=1  # Saves the model after each epoch
