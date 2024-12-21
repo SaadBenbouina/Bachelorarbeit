2b55330944a7fefed1901d88e8bf31b4cbafa4a6
saadbenboujina <saadbenboujina@gmail.com>
2024-12-21 10:38:50 +0100

    filter anpassen

--- Pipeline/ShipLabelFilter.py
+++ Pipeline/ShipLabelFilter.py
@@ -49,8 +49,46 @@ class ShipLabelFilter:
             label = 'military'
         if 'sd_14' in label:
             label = 'cargo_ship'
+        if 'cargo_ship' in label:
+            label = 'cargo_ship'
+        if 'container' in label:
+            label = 'cargo_ship'
+        if 'tank' in label:
+            label = 'cargo_ship'
+        if 'bulk' in label:
+            label = 'cargo_ship'
+        if 'reefer' in label:
+            label = 'cargo_ship'
+        if 'tanker' in label:
+            label = 'cargo_ship'
+        if 'vehicle_carrier' in label:
+            label = 'cargo_ship'
+        if 'barge' in label:
+            label = 'cargo_ship'
+        if 'ro' in label:
+            label = 'cargo_ship'
+        if 'general_cargo' in label:
+            label = 'cargo_ship'
+        if 'landing_craft' in label:
+            label = 'cargo_ship'
+        if 'cement' in label:
+            label = 'cargo_ship'
+        if 'aggregates_carrier' in label:
+            label = 'cargo_ship'
+        if 'vehicle' in label:
+            label = 'cargo_ship'
+        if 'ore' in label:
+            label = 'cargo_ship'
+        if 'dry_cargo' in label:
+            label = 'cargo_ship'
+
         if 'drill' in label:
             label = 'work_boat'
+        if 'tug' in label:
+            label = 'work_boat'
+        if 'work_boat' in label:
+            label = 'work_boat'
+
         if 'battle' in label:
             label = 'military'
         if 'auxiliar' in label:
@@ -61,63 +99,29 @@ class ShipLabelFilter:
             label = 'military'
         if 'carrier' in label:
             label = 'military'
+        if 'attack' in label:
+            label = 'military'
+        if 'landing_ship' in label:
+            label = 'military'
+
         if 'lifeboat' in label:
             label = 'rescue_boat'
+        if 'rescue' in label:
+            label = 'rescue_boat'
 
-        if 'cargo_ship' in label:
-            label = 'cargo_ship'
-        if 'container' in label:
-            label = 'cargo_ship'
-        if 'tank' in label:
-            label = 'cargo_ship'
-        if 'bulk' in label:
-            label = 'cargo_ship'
-        if 'fish' in label:
-            label = 'fishing_ship'
-        if 'cruise' in label:
-            label = 'ferry'
-        if 'reefer' in label:
-            label = 'cargo_ship'
-        if 'tanker' in label:
-            label = 'cargo_ship'
-        if 'sailing' in label:
-            label = 'sailing_ship'
-        if 'tug' in label:
-            label = 'work_boat'
-        if 'vehicle_carrier' in label:
-            label = 'cargo_ship'
-        if 'work_boat' in label:
-            label = 'work_boat'
         if 'passenger' in label:
             label = 'ferry'
         if 'ferr' in label:
             label = 'ferry'
-        if 'barge' in label:
-            label = 'cargo_ship'
-        if 'rescue' in label:
-            label = 'rescue_boat'
-        if 'attack' in label:
-            label = 'military'
-        if 'ro' in label:
-            label = 'cargo_ship'
         if 'tour' in label:
             label = 'ferry'
-        if 'cement' in label:
-            label = 'cargo_ship'
-        if 'submarine' in label:
-            label = 'submarine'
-        if 'general_cargo' in label:
-            label = 'cargo_ship'
-        if 'landing_craft' in label:
-            label = 'cargo_ship'
-        if 'landing_ship' in label:
-            label = 'military'
         if 'high_speed' in label:
             label = 'ferry'
-        if 'aggregates_carrier' in label:
-            label = 'cargo_ship'
         if 'steam' in label:
             label = 'ferry'
+        if 'cruise' in label:
+            label = 'ferry'
+
         if 'coast_guard' in label:
             label = 'order'
         if 'police' in label:
@@ -126,12 +130,16 @@ class ShipLabelFilter:
             label = 'order'
         if 'patrol' in label:
             label = 'order'
-        if 'vehicle' in label:
-            label = 'cargo_ship'
-        if 'ore' in label:
-            label = 'cargo_ship'
+
         if 'whale' in label:
             label = 'fishing_ship'
-        if 'dry_cargo' in label:
-            label = 'cargo_ship'
+        if 'fish' in label:
+            label = 'fishing_ship'
+
+        if 'submarine' in label:
+            label = 'submarine'
+
+        if 'sailing' in label:
+            label = 'sailing_ship'
+
         return label
